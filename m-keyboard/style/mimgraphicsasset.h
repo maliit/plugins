01b749879c88792ebfd84677736896eac2ca9a4e
Michael Hasselmann <michaelh@openismus.com>
2011-07-15 16:14:03 +0200

    New: MImGraphicsAsset handle class for pixmaps
    RevBy: TrustMe
    A POD type that also carries the identifier (useful for testing) and the
    non-scalable margins of a graphical asset. Together with the pixmap, this can
    be used together with Qt's qDrawBorderPixmap.

---
+++ m-keyboard/style/mimgraphicsasset.h
@@ -0,0 +1,59 @@
+/*
+ * This file is part of meego-keyboard
+ *
+ * Copyright (C) 2010-2011 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
+ *
+ * Contact: Mohammad Anwari <Mohammad.Anwari@nokia.com>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * Neither the name of Nokia Corporation nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#ifndef MIMGRAPHICSASSET_H
+#define MIMGRAPHICSASSET_H
+
+#include <QString>
+#include <QMargins>
+#include <QPixmap>
+
+//! POD read-only struct that annotates a pixmap with a human-readable
+//! identifier (also useful for tests) and margins to determine the
+//! non-scalable corners of the pixmap on a 3x3 grid.
+struct MImGraphicsAsset
+{
+public:
+    const bool isValid; //!< Invalid if either identifier or pixmap are invalid. \sa QPixmap::isNull
+    const QString identifier; //!< The identifier of this graphics asset.
+    const QMargins margins; //!< Can be used to slice pixmap into 3x3 grid. \sa qDrawBorderPixmap
+    const QPixmap pixmap; //!< The annotated pixmap.
+
+    //! C'tor
+    MImGraphicsAsset(const QString &newIdentifier,
+                     const QMargins &newMargins,
+                     const QPixmap &newPixmap);
+
+    //! Default c'tor. Constructs invalid MImGraphicsAsset instance.
+    MImGraphicsAsset();
+};
+
+#endif // MIMGRAPHICSASSET_H
--- m-keyboard/style/style.pri
+++ m-keyboard/style/style.pri
@@ -2,9 +2,11 @@ STYLE_DIR = ./style
 
 HEADERS += \
     $$STYLE_DIR/mimkeystyle.h \
+    $$STYLE_DIR/mimgraphicsasset.h \
 
 SOURCES += \
     $$STYLE_DIR/mimkeystyle.cpp \
+    $$STYLE_DIR/mimgraphicsasset.cpp \
 
 INCLUDEPATH += $$STYLE_DIR
 DEPENDPATH += $$STYLE_DIR
