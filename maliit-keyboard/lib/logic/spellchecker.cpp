ad7e3a35e5f682879342bf2c75f4300fa994b684
Michael Hasselmann <michaelh@openismus.com>
2012-04-02 21:46:37 +0200

    WIP: Remove SpellChecker implementation
    RevBy: TrustMe
    CC SA 2.5 is not compatible with BSD license, so I decided to remove
    implementation, only leaving the interface. We need to wait until license
    situation for Qt hunspell wrapper has been improved, but I don't want the word
    prediction/error correction feature to block on that.

--- maliit-keyboard/lib/logic/spellchecker.cpp
+++ maliit-keyboard/lib/logic/spellchecker.cpp
@@ -1,5 +1,33 @@
-// License would be CC SA 2.5
-// Taken from http://qt-project.org/wiki/Spell_Checking_with_Hunspell#b8c5300cf30231da0a9520d8bde7120d
+/*
+ * This file is part of Maliit Plugins
+ *
+ * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
+ *
+ * Contact: Mohammad Anwari <Mohammad.Anwari@nokia.com>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * Neither the name of Nokia Corporation nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #include "spellchecker.h"
 
@@ -22,90 +50,22 @@ namespace Logic {
 SpellChecker::SpellChecker(const QString &dictionaryPath,
                            const QString &userDictionary)
 {
-#ifndef HAVE_HUNSPELL
     Q_UNUSED(dictionaryPath)
     Q_UNUSED(userDictionary)
-#else
-    m_user_dictionary_path = userDictionary;
-
-    QString dictFile = dictionaryPath + ".dic";
-    QString affixFile = dictionaryPath + ".aff";
-    QByteArray dictFilePathBA = dictFile.toLocal8Bit();
-    QByteArray affixFilePathBA = affixFile.toLocal8Bit();
-    m_hunspell.reset(new Hunspell(affixFilePathBA.constData(), dictFilePathBA.constData()));
-
-    // detect encoding analyzing the SET option in the affix file
-    m_encoding = "ISO8859-1";
-    QFile _affixFile(affixFile);
-    if (_affixFile.open(QIODevice::ReadOnly)) {
-        QTextStream stream(&_affixFile);
-        QRegExp enc_detector("^\\s*SET\\s+([A-Z0-9\\-]+)\\s*", Qt::CaseInsensitive);
-        for(QString line = stream.readLine(); !line.isEmpty(); line = stream.readLine()) {
-            if (enc_detector.indexIn(line) > -1) {
-                m_encoding = enc_detector.cap(1);
-                qDebug() << QString("Encoding set to ") + m_encoding;
-                break;
-            }
-        }
-        _affixFile.close();
-    }
-    m_codec = QTextCodec::codecForName(this->m_encoding.toLatin1().constData());
-
-    if(!m_user_dictionary_path.isEmpty()) {
-        QFile userDictonaryFile(m_user_dictionary_path);
-        if(userDictonaryFile.open(QIODevice::ReadOnly)) {
-            QTextStream stream(&userDictonaryFile);
-            for(QString word = stream.readLine(); !word.isEmpty(); word = stream.readLine())
-                putWord(word);
-            userDictonaryFile.close();
-        } else {
-            qWarning() << "User dictionary in " << m_user_dictionary_path << "could not be opened";
-        }
-    } else {
-        qDebug() << "User dictionary not set.";
-    }
-#endif
 }
 
 bool SpellChecker::spell(const QString &word)
 {
-#ifndef HAVE_HUNSPELL
     Q_UNUSED(word)
     return true;
-#else
-    // Encode from Unicode to the encoding used by current dictionary
-    return m_hunspell->spell(m_codec->fromUnicode(word).constData()) != 0;
-#endif
 }
 
 QStringList SpellChecker::suggest(const QString &word,
                                   int limit)
 {
-#ifndef HAVE_HUNSPELL
     Q_UNUSED(word)
     Q_UNUSED(limit)
     return QStringList();
-#else
-    if (spell(word)) {
-        return QStringList();
-    }
-
-    char **suggestions_buf;
-    QStringList suggestions;
-
-    // Encode from Unicode to the encoding used by current dictionary
-    const int num_suggestions = m_hunspell->suggest(&suggestions_buf,
-                                                    m_codec->fromUnicode(word).constData());
-    const int count(limit == -1 ? num_suggestions
-                                : qMin<int>(limit, num_suggestions));
-
-    for (int index = 0; index  < count; ++index) {
-        suggestions.append(m_codec->toUnicode(suggestions_buf[index]));
-        free(suggestions_buf[index]);
-    }
-
-    return suggestions;
-#endif
 }
 
 SpellChecker::~SpellChecker()
@@ -113,37 +73,17 @@ SpellChecker::~SpellChecker()
 
 void SpellChecker::ignoreWord(const QString &word)
 {
-    putWord(word);
+    Q_UNUSED(word)
 }
 
 void SpellChecker::putWord(const QString &word)
 {
-#ifndef HAVE_HUNSPELL
     Q_UNUSED(word)
-#else
-    m_hunspell->add(m_codec->fromUnicode(word).constData());
-#endif
 }
 
 void SpellChecker::addToUserWordlist(const QString &word)
 {
-#ifndef HAVE_HUNSPELL
     Q_UNUSED(word)
-#else
-    putWord(word);
-    if(!m_user_dictionary_path.isEmpty()) {
-        QFile userDictonaryFile(m_user_dictionary_path);
-        if(userDictonaryFile.open(QIODevice::Append)) {
-            QTextStream stream(&userDictonaryFile);
-            stream << word << "\n";
-            userDictonaryFile.close();
-        } else {
-            qWarning() << "User dictionary in " << m_user_dictionary_path << "could not be opened for appending a new word";
-        }
-    } else {
-        qDebug() << "User dictionary not set.";
-    }
-#endif
 }
 
 }} // namespace Logic, MaliitKeyboard
